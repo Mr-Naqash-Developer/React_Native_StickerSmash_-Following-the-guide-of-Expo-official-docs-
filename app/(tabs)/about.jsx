493cc4e36a9c4ce117ead248e0839ca15cc8fc11
Mr.Naqash.Developer <mr.naqash.developer@gmail.com>
2025-03-17 10:56:42 +0500

    I've completed the ch#2 from the official expo docs

--- app/(tabs)/about.jsx
+++ app/(tabs)/about.jsx
@@ -1,10 +1,13 @@
 import { View, Text, StyleSheet } from 'react-native'
-import React from 'react'
+import { Link } from 'expo-router'
 
 const about = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>About Screen</Text>
+      <Link href="/" style={styles.button} >
+        Back to home
+      </Link>
     </View>
   )
 }
@@ -19,6 +22,11 @@ const styles = StyleSheet.create({
   text: {
     color: '#fff'
   },
+  button: {
+    fontSize: 20,
+    textDecorationLine: 'underline',
+    color: "#fff"
+  }
 })
 
 
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,13 @@
 import { View, Text, StyleSheet } from 'react-native'
-import React from 'react'
+import { Link } from 'expo-router'
 
 const index = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Home Screen</Text>
+      <Link href='/about' style={styles.button}>
+        Go to About Screen
+      </Link>
     </View>
   )
 }
@@ -18,6 +21,11 @@ const styles = StyleSheet.create({
   },
   text: {
     color: '#fff'
+  },
+  button: {
+    fontSize: 20,
+    textDecorationLine: 'underline',
+    color: "#fff"
   },
 })
 
